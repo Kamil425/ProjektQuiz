39d17a29e0339446b3932e01b42f07e43abcc011
Kamil425 <128582000+Kamil425@users.noreply.github.com>
2023-10-10 22:02:42 +0200

    Dodanie JakToDziała/poprawki navbar i str.głównej

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,10 +14,10 @@ const Navbar = () => {
                     <div className = "h-4/6 w-2/12"><Image src={Logo} alt="Logo" className='h-full w-full' width="250" height="150" onClick={StartPage}/></div>
                     <div className='w-5/12 h-full'></div>
                     <div className='h-full w-6/12 justify-end flex flex-row items-center text-xl font-bold'>
-                        <p className='w-1/4 mr-6 text-four hover:text-secondary transition-all duration-300'>Jak to działa?</p>
-                        <p className='w-1/4 mr-6 text-four hover:text-secondary transition-all duration-300'>Features</p>
-                        <Link href="/about" className='w-1/4 mr-6 text-four hover:text-secondary transition-all duration-300'><p >O nas</p></Link>
-                        <button className='mr-6 h-1/2 w-1/4 text-three border-solid border-2 border-three'>Login</button>
+                        <Link href="/howwork" className='w-2/4 mr-6 2xl:text-l xl:text-l l:text-s md:text-s sm:text-s max-sm:text-xs text-four hover:text-secondary transition-all duration-300'><p>Jak to działa?</p></Link>
+                        <p className='w-2/4 mr-6 2xl:text-l xl:text-l l:text-s md:text-s sm:text-s max-sm:text-xs text-four hover:text-secondary transition-all duration-300'>Features</p>
+                        <Link href="/about" className='w-2/4 mr-6 2xl:text-l xl:text-l l:text-s md:text-s sm:text-s max-sm:text-xs text-four hover:text-secondary transition-all duration-300'><p >O nas</p></Link>
+                        <button className='mr-6 h-1/2 w-2/4 2xl:text-l xl:text-l l:text-s md:text-s sm:text-s max-sm:text-xs text-three border-solid border-2 border-three'>Login</button>
                     </div>
                     </div>
                 <hr className="h-2 w-full bg-primary border-none"></hr>
--- app/components/Startpage.tsx
+++ app/components/Startpage.tsx
@@ -9,11 +9,10 @@ const StartPage = () => {
     return ( 
         <div className="h-full w-5/6 flex flex-row">
             <div className="h-full w-1/2 flex flex-col">
-                <div className="h-1/2 w-1/2 flex flex-wrap pt-6"><h1 className="h-full w-full 2xl:text-6xl xl:text-5xl l:text-4xl md:text-4xl sm:text-2xl max-sm:text-xl font-semibold">Poszerzaj swoją wiedzę razem z nami</h1></div>
-                <div className="h-1/4 w-1/2"><p className="border-l-2 pl-4 text-lg sm:text-md">Pomożemy Ci przygotować się do egzaminów</p></div>
+                <div className="h-1/2 w-1/2 flex flex-wrap pt-6"><h1 className="h-full w-full 2xl:text-6xl xl:text-5xl l:text-4xl md:text-4xl sm:text-2xl max-sm:text-2xl font-semibold">Poszerzaj swoją wiedzę razem z nami</h1></div>
+                <div className="h-1/4 w-1/2"><p className="border-l-2 pl-4 2xl:text-xl xl:text-xl l:text-l md:text-l sm:text-s max-sm:text-s">Pomożemy Ci przygotować się do egzaminów</p></div>
                 <div className="h-1/4 w-1/2 flex">
-                    <button className="h-1/2 w-1/2 bg-three text-white shadow-md shadow-three text-lg" onClick={QuizChoice}>Zacznij naukę</button>
-        
+                <button className="h-1/2 w-3/4 bg-three text-white shadow-md shadow-three 2xl:text-xl xl:text-xl l:text-l md:text-l sm:text-s max-sm:text-xs" onClick={QuizChoice}>Zacznij naukę</button>
                     </div>
                 </div>
             <div className="h-full w-1/2"><Image src={MainPage} alt="MainPage" className='h-full w-full'/>
@@ -21,5 +20,5 @@ const StartPage = () => {
         </div>
      );
 }
- 
+
 export default StartPage;
