f8ed872ab21da67fb131540573a6b893185ba125
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2023-10-16 13:16:29 +0200

    Add files via upload

---
+++ app/rejestracja/page.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Rejestracja = () => {
+    return (
+  
+  <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+      <div style={{width: '409.30px', height: '594.50px', left: '426.72px', top: '0px', position: 'absolute'}}>
+          <div style={{width: '409.30px', height: '594.50px', left: '0px', top: '0px', position: 'absolute', background: 'radial-gradient(66.83% 69.65% at 180.56% 162.18%, rgba(255, 255, 255, 0.41) 17%, rgba(243.31, 243.31, 243.31, 0.41) 33%)'}}></div>
+          <img style={{width: '286.22px', height: '231.65px', left: '61.54px', top: '181.14px', position: 'absolute', boxShadow: '0px 2.3222765922546387px 14.514228820800781px rgba(0, 0, 0, 0.25)'}} src="https://via.placeholder.com/286x232" />
+      </div>
+      <div style={{width: '239.19px', height: '33.67px', left: '73.18px', top: '109.47px', position: 'absolute', textAlign: 'center', color: 'rgba(0, 0, 0, 0.60)', fontSize: '14.43px', fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word'}}>Welcome!<br/> Please create your account.</div>
+      
+      
+      <div style={{width: '347.76px', height: '42.96px', left: '31.35px', top: '150.68px', position: 'absolute'}}>
+          <div style={{width: '347.76px', height: '62.96px', left: '0px', top: '0px', position: 'absolute', background: 'white', border: '0.58px #C1BBBB solid'}}></div>
+          <div style={{width: '51.80px', height: '0px', left: '2.51px', top: '62.38px', position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', background: '#FCC822', border: '2.90px #FCC822 solid'}}></div>
+          <div style={{left: '16.95px', top: '6.97px', position: 'absolute', color: 'rgba(0, 0, 0, 0.61)', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word'}}>User Name</div>
+          <input className="poleNazwa" style={{ left: '16.95px', top: '22.64px', position: 'absolute', color: '#FCC822', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '500', wordWrap: 'break-word'}}></input>
+      </div>
+      
+      <div style={{width: '82.44px', left: '31.35px', top: '311.77px', position: 'absolute'}}>
+          <input className='checkboxRejestracja' type='checkbox' style={{width: '8.71px', height: '8.71px', left: '0px', top: '1.74px', position: 'absolute', background: 'white', border: '0.58px #B4B4B4 solid'}}></input>
+          <div style={{width: '68.51px', height: '12.19px', left: '13.93px', top: '0px', position: 'absolute', color: 'rgba(0, 0, 0, 0.61)', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word'}}>Remember Me</div>
+      </div>
+      
+      <div style={{width: '347.76px', height: '42.96px', left: '31.35px', top: '269.06px', position: 'absolute', background: 'white', border: '0.58px #C1BBBB solid'}}></div>
+      <div style={{width: '46.45px', height: '12.19px', left: '47.03px', top: '276.03px', position: 'absolute', color: 'rgba(0, 0, 0, 0.61)', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word'}}>Password</div>
+      <input className="poleHasloRejestracja" type='password' style={{width: '88.25px', height: '12.19px', left: '47.03px', top: '291.71px', position: 'absolute', color: '#FCC822', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '500', wordWrap: 'break-word'}}></input>
+      <div style={{width: '347.76px', height: '42.96px', left: '31.35px', top: '206.68px', position: 'absolute'}}>
+          <div style={{width: '347.76px', height: '62.96px', left: '0px', top: '0px', position: 'absolute', background: 'white', border: '0.58px #C1BBBB solid'}}></div>
+          <div style={{width: '51.80px', height: '0px', left: '2.51px', top: '62.38px', position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', background: '#FCC822', border: '2.90px #FCC822 solid'}}></div>
+          <div style={{left: '16.95px', top: '6.97px', position: 'absolute', color: 'rgba(0, 0, 0, 0.61)', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word'}}>Email Address</div>
+          <input className="poleEmailRejestracja" style={{ left: '16.95px', top: '22.64px', position: 'absolute', color: '#FCC822', fontSize: '10.45px', fontFamily: 'Roboto', fontWeight: '500', wordWrap: 'break-word'}}></input>
+      </div>
+      <button className="ButtonSignupRejestracja" style={{width: '71.41px', height: '37.16px', left: '31.35px', top: '370.40px', position: 'absolute', background: 'orange', color: 'white', fontSize: '11.61px', fontFamily: 'Poppins', fontWeight: '500', wordWrap: 'break-word'}}>Signup
+           </button>
+     
+         
+     
+      <div style={{width: '248.49px', height: '53.32px', left: '68.51px', top: '38.32px', position: 'absolute'}}>
+          <div style={{width: '234.56px', height: '53.32px', left: '13.93px', top: '0px', position: 'absolute'}}><span style={{color: '#3B3B3B', fontSize: '45.70px', fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Quiz</span><span style={{color: '#FCC822', fontSize: '45.70px', fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Grad</span></div>
+          <img style={{width: '35.38px', height: '25.92px', left: '0px', top: '12.97px', position: 'absolute', transform: 'rotate(-21.50deg)', transformOrigin: '0 0'}} src="https://via.placeholder.com/35x26" />
+      </div>
+  </div>
+  
+  
+  );
+  }
+  
+  export default Rejestracja;
